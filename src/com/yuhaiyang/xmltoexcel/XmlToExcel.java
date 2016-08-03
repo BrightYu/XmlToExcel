794029e5172061c57f0bb807b0080662d173b5da
公司电脑 <yuhaiyang@company.com>
2016-08-03 16:00:45 +0800

    update

--- src/com/yuhaiyang/xmltoexcel/XmlToExcel.java
+++ src/com/yuhaiyang/xmltoexcel/XmlToExcel.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2016 The yuhaiyang Android Source Project
- * <p>
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,8 +22,6 @@ public class XmlToExcel {
 
     public static void main(String[] args) throws Exception {
         System.out.printf("main");
-//        List<Cistern> datas = XmlUtils.parse("D://strings.xml");
-//        ExcelUtils.create(datas);
         MainUI tabTest = new MainUI();
         tabTest.initUI();
     }
